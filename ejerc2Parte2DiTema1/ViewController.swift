429523413cfb4842fb28d0c3c5eec602ed4effd7
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-11-02 09:15:47 +0100

    Initial Commit

---
+++ ejerc2Parte2DiTema1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejerc2Parte2DiTema1
+//
+//  Created by Montacer El Fazazi on 02/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
