e1cf778e43e4dd507a0bec6fe08e59fac1645db8
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-11-02 10:20:13 +0100

    good

--- ejerc2Parte2DiTema1/ViewController.swift
+++ ejerc2Parte2DiTema1/ViewController.swift
@@ -7,18 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ObtenerExamen { //ObtenerExamen a mano q es protocolo q hemos creado
 
     @IBOutlet weak var txtIndice: UITextField!
     @IBOutlet weak var lbResultado: UILabel!
     
+    var listaExamenes: [Examen]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        listaExamenes = []
     }
 
 
     @IBAction func btnVerExamen(_ sender: Any) {
+        let indice = txtIndice.text!
+        
+        if !indice.isEmpty && Int(indice)! >= 1 &&  Int(indice)! >= listaExamenes.count{
+        lbResultado.text = listaExamenes[Int(indice)! - 1].toString()
+        }else{
+            lbResultado.text = "ERROR en el indice"
+        }
+    }
+    
+    func obtener(dato: Examen) {
+        listaExamenes.append(dato)
+    }
+    
+    //prepare + elegir la opcion del segue + tabulador
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EXAMEN" {
+            let destino = segue.destination as!
+                InsertarExamenViewController
+            destino.delegate = self
+        }
     }
 }
 
